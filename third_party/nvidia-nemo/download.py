272fed816e355560bbd88abf67faa014afbcbe46
wanghr01 <h6116983@gmail.com>
2025-11-24 01:32:05 +0800

    Update download.py

--- third_party/nvidia-nemo/download.py
+++ third_party/nvidia-nemo/download.py
@@ -1,10 +1,16 @@
 import nemo.collections.asr as nemo_asr
 
-# 1. 加载模型（默认会缓存到 ~/.cache/torch/NeMo）
-model = nemo_asr.models.ASRModel.from_pretrained("nvidia/stt_en_conformer_transducer_large")
+# 加载模型（阿拉伯语）
+model = nemo_asr.models.ASRModel.from_pretrained("nvidia/stt_ar_fastconformer_hybrid_large_pcd_v1.0")
+custom_dir = "/root/shared-nvme/haoranwang/nemo_asr" # 自定义保存路径
+model.save_to(f"{custom_dir}/stt_ar_fastconformer_hybrid_large_pcd_v1.0.nemo")
 
-# 2. 自定义保存路径
-custom_dir = "/root/shared-nvme/haoranwang/nemo_asr"
+# 加载模型（韩语）
+model = nemo_asr.models.ASRModel.from_pretrained("eesungkim/stt_kr_conformer_transducer_large")
+custom_dir = "/root/shared-nvme/haoranwang/nemo_asr" # 自定义保存路径
+model.save_to(f"{custom_dir}/stt_kr_conformer_transducer_large")
 
-# 3. 保存模型到指定目录（生成 .nemo 文件）
-model.save_to(f"{custom_dir}/stt_en_conformer_transducer_large.nemo")
+# 加载模型（日语）
+model = nemo_asr.models.ASRModel.from_pretrained("nvidia/parakeet-tdt_ctc-0.6b-ja")
+custom_dir = "/root/shared-nvme/haoranwang/nemo_asr" # 自定义保存路径
+model.save_to(f"{custom_dir}/parakeet-tdt_ctc-0.6b-ja")
