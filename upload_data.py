c339db549807ee9184e0ce3f518351d5a097dd50
G D Lin <Lingd20>
2025-11-25 14:34:28 +0800

    dolphin

--- upload_data.py
+++ upload_data.py
@@ -6,13 +6,13 @@ api = HfApi(token=HF_TOKEN)
 
 # 注意这里最好是保持结构一致，即文件放在text/hyp/testbatch下，上传test文件夹
 
-LOCAL_FOLDER = "/root/shared-nvme/yujietu/data/ASR-Bench/text" 
+LOCAL_FOLDER = "E:/Desktop/master/master_3/benchmark/result_revision" 
 
 api.upload_folder(
     folder_path=LOCAL_FOLDER,
     repo_id="AlexTYJ/Multilingual-ASR-Benchmark",
     repo_type="dataset",
-    path_in_repo="testbatch",
+    path_in_repo="text/hyp/testbatch",
 )
 
 print("Upload Done!")
