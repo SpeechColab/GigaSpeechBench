15ab972da908f7df31b742f1a6bed3184ff4499a
AlexTYJ <alextuyujie@gmail.com>
2025-11-27 01:43:30 +0800

    update IRQ.py

--- text_norm/IRQ.py
+++ text_norm/IRQ.py
@@ -1,48 +1,51 @@
 import regex as re
 
 def normalize(text: str) -> str:
+ """
+    Arabic text normalization:
+    1. Remove punctuation
+    2. Remove diacritics
+    3. Eastern Arabic numerals to Western Arabic numerals
+
+    Arguments
+    ---------
+    text: str
+        text to normalize
+    Output
+    ---------
+    normalized text
     """
-    输入：原始文本
-    输出：text_norm 后的文本
-    """
+    # Remove punctuation
     text = re.sub(r"[\p{p}\p{s}]", "", text)
 
-    diacritics = r"[\u064b-\u0652]"
+    # Remove diacritics
+    diacritics = r'[\u064B-\u0652]'  # Arabic diacritical marks (Fatha, Damma, etc.)
+    text = re.sub(diacritics, '', text)
+    
+    # Normalize Hamzas and Maddas
+    text = re.sub('پ', 'ب', text)
+    text = re.sub('ڤ', 'ف', text)
+    text = re.sub(r'[آ]', 'ا', text)
+    text = re.sub(r'[أإ]', 'ا', text)
+    text = re.sub(r'[ؤ]', 'و', text)
+    text = re.sub(r'[ئ]', 'ي', text)
+    text = re.sub(r'[ء]', '', text)   
 
-    text = re.sub(diacritics, "", text)
-
-    text = re.sub("پ", "ب", text)
-    text = re.sub("ڤ", "ف", text)
-
-    text = re.sub(r"[آ]", "ا", text)
-    text = re.sub(r"[أإ]", "ا", text)
-    text = re.sub(r"[ؤ]", "و", text)
-    text = re.sub(r"[ئ]", "ي", text)
-    text = re.sub(r"[ء]", "", text)
-
-    text = text.replace("ى", "ي")
-
-    text = text.replace("اً", "ا")
-
-    text = re.sub(r"ه\b", "ة", text)
-
-    western_to_eastern = {
-        "0": "٠",
-        "1": "١",
-        "2": "٢",
-        "3": "٣",
-        "4": "٤",
-        "5": "٥",
-        "6": "٦",
-        "7": "٧",
-        "8": "٨",
-        "9": "٩",
+    # Transliterate Eastern Arabic numerals to Western Arabic numerals
+    eastern_to_western_numerals = {
+        '٠': '0', '١': '1', '٢': '2', '٣': '3', '٤': '4', 
+        '٥': '5', '٦': '6', '٧': '7', '٨': '8', '٩': '9'
     }
-    for en, ar in western_to_eastern.items():
-        text = text.replace(en, ar)
+    for eastern, western in eastern_to_western_numerals.items():
+        text = text.replace(eastern, western)
 
+    # Remove tatweel (kashida, u+0640)
     text = re.sub(r"\u0640", "", text)
-
-    text = re.sub(r"\s\s+", " ", text)
+    
+    # Remove hmm-uhm-like words
+    text = re.sub(r"اا+", "", text)
+    
+    # Normalize multiple whitespace characters into a single space
+    text = re.sub(r'\s\s+', ' ', text)
 
     return text.strip()
