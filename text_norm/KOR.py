6d137ed8c7fed42c01f9b81d1a697828d2fa677c
Yifan Yang <64255737+yfyeung@users.noreply.github.com>
2025-11-30 16:39:57 +0800

    Add files via upload

---
+++ text_norm/KOR.py
@@ -0,0 +1,32 @@
+import re
+import unicodedata
+
+
+def normalize(text: str) -> str:
+    text = unicodedata.normalize("NFC", text)
+
+    text = re.sub(r"\[.*?\]", " ", text)
+
+    korean_english_only_pattern = re.compile(
+        r"[^"
+        r"\uAC00-\uD7A3"  # Hangul Syllables (现代音节)
+        r"\u1100-\u11FF"  # Hangul Jamo (组合字母)
+        r"\u3130-\u318F"  # Hangul Compatibility Jamo (兼容字母, 如 ㅋㅋ)
+        r"\uA960-\uA97F"  # Hangul Jamo Extended-A
+        r"\uD7B0-\uD7FF"  # Hangul Jamo Extended-B
+        r"a-zA-Z"  # English Alphabets
+        r"]"
+    )
+
+    text = korean_english_only_pattern.sub("", text)
+
+    text = text.upper()
+
+    return text
+
+
+if __name__ == "__main__":
+    import sys
+
+    ori_text = sys.argv[1]
+    print(normalize(ori_text))
